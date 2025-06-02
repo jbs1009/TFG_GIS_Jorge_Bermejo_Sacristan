bf372eea19e3f36bbcca70a79724a72a8dc5c80a
jbs1009 <jbs1009@alu.ubu.es>
2025-06-02 12:51:21 +0200

    Código corrige_csfastq.py comentado

--- corrige_csfastq.py
+++ corrige_csfastq.py
@@ -1,9 +1,32 @@
 import sys
 
 def arregla_qual(calidad):
+    """
+    Desecha la posición inicial sobrante de la calidad de un archivo CSFASTQ.
+    
+    Args
+    ---------
+    calidad: str, cadena correspondiente a la calidad original de un archivo CSFASTQ.
+
+    Returns
+    ---------
+    str, cadena excluyendo la primera posición de la calidad
+    """
     return calidad[1:]
 
 def corrige_csfastq(archivo_csfastq, archivo_csfastq_corregido):
+    """
+    Función que corrige un archivo en formato CSFASTQ eliminando la posición inicial sobrante de las líneas correspondientes a su calidad.
+
+    Args
+    ---------
+    archivo_csfastq: ruta del archivo en formato CSFASTQ que se desea corregir, pasado como entrada
+    archivo_csfastq_corregido: ruta de destino del archivo en formato CSFASTQ, una vez corregido
+
+    Returns
+    ---------
+    None
+    """
 
     with open(archivo_csfastq, "r") as archivo_csfastq, open(archivo_csfastq_corregido, "w") as archivo_csfastq_corregido:
 
@@ -18,6 +41,7 @@ def corrige_csfastq(archivo_csfastq, archivo_csfastq_corregido):
             separador = archivo_csfastq.readline().strip()
             calidad = archivo_csfastq.readline().strip()
 
+            #Aplica la función arregla_qual() sobre la línea de calidad a corregir
             calidad_corregida = arregla_qual(calidad)
 
             archivo_csfastq_corregido.write(f"{identificador}\n{secuencia}\n{separador}\n{calidad_corregida}\n")
